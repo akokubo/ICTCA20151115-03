2da400212a76a2220189f588c2eeb09f8fd51563
Atsushi Kokubo <kokubo@aomori-u.ac.jp>
2015-11-16 18:55:53 +0900

    ボタンをクリックするとボタンのテキストを変更

--- app/src/main/java/com/android_lesson/aomori/hello3/MainActivity.java
+++ app/src/main/java/com/android_lesson/aomori/hello3/MainActivity.java
@@ -2,6 +2,8 @@ package com.android_lesson.aomori.hello3;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btn = (Button)findViewById(R.id.button);
+        btn.setText("HELLO");
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Button b = (Button) v;
+                b.setText("こんにちは！");
+            }
+        });
     }
 }
